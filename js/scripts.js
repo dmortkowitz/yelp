ca9e8929be10319144a3060a901a440e2b578ff4
davidmortkowitz <dmortkowitz@gmail.com>
2018-08-08 23:00:42 -0700

    Add hidden number and reveal function through Javascript, CSS and HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   $("#addContact").submit(function(event){
     var name = $("#name").val();
     var phoneNum = $("#phoneNum").val();
-    $("#contactList").append('<li class="list-group-item name"><span>'+ name +'</span></li><li class="list-group-item phoneNum"><span>'+phoneNum+'</span></li>')
+    $("#contactList").append('<li class="list-group-item name"><span>'+ name +'</span></li><li class="list-group-item phoneNum" id="phoneclick"><span>'+phoneNum+'</span></li>')
 
-    $("#contactList li .name").click(function(){
-      $(this).next().toggle();
+    $("#name").val("");
+    $("#phoneNum").val("")
+
+    $(".name").last().click(function(){
+      $(this).next().show();
+
+
     });
 
     event.preventDefault();
